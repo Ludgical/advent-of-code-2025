ed38da7e06c122e5ec5c673b1363e187db91b1e4
Ludgical <lud.erik.10@gmail.com>
2025-12-07 14:28:19 +0100

    Day 7

--- src/day07/Day07.kt
+++ src/day07/Day07.kt
@@ -27,11 +27,16 @@ fun part1(grid: Array<Array<Char>>, startCoord: Pair<Int, Int>, splitterCoords: 
 
     for (splitterCoord in splitterCoords) {
         if (splitterCoord.second != 2) {
+            //Every splitter except for the one under the start
 
             for (y in splitterCoord.second - 2 downTo 2 step 2) {
+                // If another splitter is directly over the current splitter,
+                // the current splitter can't be reached
                 if (grid[splitterCoord.first][y] == '^')
                     break
 
+                // If another splitter is above the current splitter to the right or left,
+                // and that splitter has been reached, the current splitter can be reached
                 if (grid[splitterCoord.first + 1][y] == '^') {
                     if (reachedSplitterCoords.contains(Pair(splitterCoord.first + 1, y))) {
                         reachedSplitterCoords.add(splitterCoord)
@@ -54,15 +59,23 @@ fun part1(grid: Array<Array<Char>>, startCoord: Pair<Int, Int>, splitterCoords: 
 }
 
 fun part2(grid: Array<Array<Char>>, splitterCoords: MutableList<Pair<Int, Int>>): Long {
+    // Map every splitter to the amount of new worlds that get created after a beam gets split on it
+
     val newWorldsForSplitter = mutableMapOf<Pair<Int, Int>, Long>()
 
+    // The y coordinate of the lowest splitter
     val maxY: Int = splitterCoords.last().second
 
+    // Start at the bottom
     for (splitterCoord in splitterCoords.reversed()) {
+        // If the splitter is at the bottom, set the new world amount of the splitter to 1
         if (splitterCoord.second == maxY)
             newWorldsForSplitter[splitterCoord] = 1
 
         else {
+            // If the splitter is not at the bottom, set the new world amount of the splitter to
+            // the sum of the new world amounts of the splitters that are down to the right and
+            // down to the left of the splitter
             var newWorlds = 1L
             for (x in setOf(splitterCoord.first - 1, splitterCoord.first + 1))
                 for (y in splitterCoord.second + 2..maxY step 2)
